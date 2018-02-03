b30d95dbab2ec7fb730f37b7711055dc171d2d83
Runmin <yangrun@iupui.edu>
2018-02-02 23:39:10 -0500

    Read mzML file using the node.js and ajax

--- src/test/read_file/node/server.js
+++ src/test/read_file/node/server.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
- 
+var exec = require('child_process').exec;
+var filePath = ""
+
 // 创建 application/x-www-form-urlencoded 编码解析
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
  
@@ -11,14 +13,28 @@ app.get('/index.html', function (req, res) {
    res.sendFile( __dirname + "/" + "index.html" );
 })
  
-app.post('/process_post', urlencodedParser, function (req, res) {
- 
-   // 输出 JSON 格式
-   var response = {
-       "filePath":req.body.filePath
-   };
-   console.log(response);
-   res.end(JSON.stringify(response));
+app.post('/file_path_post', urlencodedParser, function (req, res) {
+  filePath = "./" + req.body.filePath
+  var cmd = "./msReader " + filePath + " -s 1";
+  exec(cmd,function(error, stdout,stderr) {
+    // get the output
+    // console.log('Success execute the demo file.\n'+stdout);
+
+   res.write(stdout);
+   console.log(stdout);
+   res.end();
+  })
+})
+app.post('/scan_post', urlencodedParser, function (req, res) {
+  var cmd = "./msReader ./" + filePath + " " + req.body.nowScanIndex;
+  exec(cmd,function(error, stdout,stderr) {
+    // get the output
+    // console.log('Success execute the demo file.\n'+stdout);
+
+   res.write(stdout);
+   console.log(stdout);
+   res.end();
+  })
 })
  
 var server = app.listen(8081, function () {
